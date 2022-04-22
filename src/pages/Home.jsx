6dfa5056110fbb44bedcf0809544b0c1e42ee646
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-22 01:58:53 -0300

    feat: importing the footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Main from '../components/Main';
 
 function Home() {
   return (
-    <div>
+    <>
       <Header />
       <Main />
-    </div>
+      <Footer />
+    </>
   );
 }
 
