07ea8b4ea5cf174cdafa01ce5bdbe166a351b394
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-22 11:48:08 -0300

    feat: implementing the state and making the post request

--- src/components/  ParentForm/index.jsx
+++ src/components/  ParentForm/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import styles from './styles.module.scss';
 
 function ParentForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      nome: name,
+      email,
+    };
+    axios.post('https://api.jungledevs.com/api/v1/challenge-newsletter/', data)
+      .then(() => {
+        setEmail('');
+        setName('');
+      })
+      .catch((error) => {
+        setEmail('');
+        setName('');
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  };
   return (
     <article>
       <section className={styles.container}>
@@ -11,10 +33,26 @@ function ParentForm() {
           Leave us your name and email and we’ll
           update you as soon as a share becomes available in your area!
         </p>
-        <form>
-          <input type="text" id="name" name="name" placeholder="Your name" />
-          <input type="text" id="email" name="email" placeholder="Your email" />
-          <button type="button">Send</button>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+            placeholder="Your name"
+            required
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            placeholder="Your email"
+            required
+          />
+          <button type="submit">Send</button>
         </form>
         <div />
       </section>
