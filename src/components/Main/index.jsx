b917c05a1663e1cde0f80efedc392b7d7b9633c1
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-21 22:48:08 -0300

    feat: importing Payments and adding main tag

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ParentForm from '../  ParentForm';
 import CreateAndManageTheTribe from '../ CreateAndManageTheTribe';
+import Payments from '../ Payments';
 import DaycareSectionAvailableNow from '../DaycareSectionAvailableNow';
 import styles from './styles.module.scss';
 
 function Main() {
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <DaycareSectionAvailableNow />
       <CreateAndManageTheTribe />
       <ParentForm />
-    </div>
+      <Payments />
+    </main>
   );
 }
 
